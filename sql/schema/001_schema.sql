d1df690bbd413679d4273e6f350f36e873507027
risenjapanllc-rgb <risen.japan.llc@gmail.com>
2026-07-08 11:15:05 +0900

    Update 001_schema.sql

--- sql/schema/001_schema.sql
+++ sql/schema/001_schema.sql
@@ -1,2 +1,54 @@
--- RISEN Knowledge OS
--- Phase1 Schema
+【ファイル名】
+
+001-schema.sql
+
+【保存場所】
+
+sql/schema/
+
+【目的】
+
+Supabaseで実行する全テーブルのCREATE TABLE文を作成してください。
+
+【作成するテーブル】
+
+Observation
+- users
+- events
+- event_categories
+- event_types
+- event_mapper
+
+Review
+- review_types
+- event_type_review_types
+
+Knowledge
+- knowledge_categories
+- knowledge_nodes
+- knowledge_relations
+- event_type_knowledge
+
+【含める内容】
+
+・CREATE TABLE
+・PRIMARY KEY
+・FOREIGN KEY
+・UNIQUE
+・NOT NULL
+・created_at
+・updated_at
+
+【含めない内容】
+
+・INSERT文
+・RLS
+・Policies
+・Triggers
+・Functions
+・Views
+・Seedデータ
+
+【完成条件】
+
+Supabase SQL Editorへそのまま貼り付けて実行できる完成版SQL
